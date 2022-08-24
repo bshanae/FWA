f585b7d0d7e4aaec06b5be7f354eb20cc108c1fc
Danny Tituaña <davila.tituanya@gazprombank.ru>
2022-08-24 20:39:35 +0300

    testing

--- src/main/java/edu/school21/cinema/servlets/ProfileServlet.java
+++ src/main/java/edu/school21/cinema/servlets/ProfileServlet.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet("/profile")
 public class ProfileServlet extends CinemaServlet {
@@ -30,5 +32,11 @@ public class ProfileServlet extends CinemaServlet {
         request.setAttribute("lastName", user.getLastName());
         request.setAttribute("phone", user.getPhoneNumber());
         request.getRequestDispatcher(jspProfile).forward(request, response);
+
+        ServletOutputStream out = response.getOutputStream();
+        out.write("Getting user data".getBytes(StandardCharsets.UTF_8));
+        out.flush();
+        out.close();
+
     }
 }
