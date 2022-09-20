42c9db2f6a74449c29b2a92b6d9a9cc7c3b3f60c
Danny Tituaña <davila.tituanya@gazprombank.ru>
2022-09-20 21:18:18 +0300

    Add test data on data sql

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,8 @@
-INSERT INTO USERS (first_name, last_name, phone_number, password)
-VALUES ( 'admin', 'admin', '1234', '1234');
+INSERT INTO USERS (first_name, last_name, phone_number, password, email, img)
+VALUES ( 'admin', 'admin', '1234', '1234', 'admin@mail.com', '');
+
+INSERT INTO USERS (first_name, last_name, phone_number, password, email, img)
+VALUES ( 'test', 'test', '1', '1', 'test@mail.com', '');
+
+INSERT INTO USERS (first_name, last_name, phone_number, password, email, img)
+VALUES ( 'Jhon', 'Connor', '4', '4', 'terminator@mail.com', 'wrong.jpg');
